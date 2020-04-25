fcf10b53f8999606c2f5798453cd30da61f1b4c6
Grady Ward <gradyward@Gradys-MacBook-Pro.local>
2020-04-25 07:08:55 -0600

    Adds CreateProjectCommand

---
+++ create_project/create_project.go
@@ -0,0 +1,106 @@
+package create_project
+
+import (
+	"encoding/json"
+	"errors"
+	"github.com/gbdubs/ecology/ecology_manifest"
+	"github.com/gbdubs/ecology/output"
+	"github.com/gbdubs/ecology/project_manifest"
+	"regexp"
+	"strings"
+)
+
+type CreateProjectCommand struct {
+	EcologyManifest     ecology_manifest.EcologyManifest
+	ProjectPath         string
+	ProjectSimpleName   string
+	ProjectManifestPath string
+	LambdaName          string
+	Platform            string
+}
+
+func (cp CreateProjectCommand) Execute(o *output.Output) (err error) {
+	o.Info("Inferring Unspecified Values for CreateProjectCommand").Indent()
+	cp = applyConstantTransformations(cp)
+	o.Dedent().Done()
+	o.Info("Validating CreateProjectComand").Indent()
+	err = cp.validate()
+	if err != nil {
+		o.Error(err)
+		return
+	}
+	o.Dedent().Done()
+
+	o.Info("Running CreateProjectCommand").Indent()
+
+	manifest := project_manifest.ProjectManifest{
+		ProjectManifestPath: cp.ProjectManifestPath,
+		ProjectName:             cp.ProjectSimpleName,
+	}
+	err = manifest.Save(o)
+	o.Dedent().Done()
+	return
+}
+
+const alphanumericRegex = "^[a-zA-Z0-9]*$"
+const alphanumericWithSlashesRegex = "^[a-zA-Z0-9/]*$"
+
+// Whether the given platform is currently supported
+var platforms = map[string]bool{"GCP": false, "AWS": true}
+
+func applyConstantTransformations(cp CreateProjectCommand) CreateProjectCommand {
+	if cp.ProjectSimpleName == "" && cp.ProjectPath != "" {
+		splits := strings.Split(cp.ProjectPath, "/")
+		cp.ProjectSimpleName = splits[len(splits)-1]
+	} else if cp.ProjectPath == "" && cp.ProjectSimpleName != "" {
+		cp.ProjectPath = cp.ProjectSimpleName
+	}
+	cp.ProjectManifestPath = cp.EcologyManifest.EcologyProjectsDirectoryPath + "/" + cp.ProjectPath + "/ecology.ecology"
+	return cp
+}
+
+func (cp CreateProjectCommand) validate() (err error) {
+	if cp.ProjectSimpleName == "" && cp.ProjectPath == "" {
+		return errors.New("create_project requires --project_name or --project_path")
+	}
+	match, _ := regexp.MatchString(alphanumericRegex, cp.ProjectSimpleName)
+	if !match {
+		return errors.New("--project_name can only contain alphanumeric characters")
+	}
+	match, _ = regexp.MatchString(alphanumericWithSlashesRegex, cp.ProjectPath)
+	if !match {
+		return errors.New("--project_path can only contain alphanumeric characters or slashes")
+	}
+	
+	if cp.LambdaName == "" {
+		return errors.New("create_project requires --lambda_name")
+	}
+	match, _ = regexp.MatchString(alphanumericRegex, cp.LambdaName)
+	if !match {
+		return errors.New("--lambda_name can only contain alphanumeric characters")
+	}
+
+	_, isEnumeratedPlatform := platforms[cp.Platform]
+	if !isEnumeratedPlatform {
+		return errors.New("--platform should be one of AWS or GCP")
+	}
+	if !platforms[cp.Platform] {
+		return errors.New("--platform=GCP is not yet supported for this command")
+	}
+
+	for _, otherProjectManifestPath := range cp.EcologyManifest.ProjectManifestPaths {
+		if otherProjectManifestPath == cp.ProjectManifestPath {
+			return errors.New("Already a project at " + cp.ProjectManifestPath)
+		}
+	}
+
+	return nil
+}
+
+func (cp CreateProjectCommand) ToString() string {
+	out, err := json.Marshal(cp)
+	if err != nil {
+		panic(err)
+	}
+	return string(out)
+}
